0c96b3aa88c4cd0bf8f2995dcc9cd5405aa3af19
sujin shin <shinsj5037@gmail.com>
2024-01-28 16:32:26 +0900

    header 로고 변경, 검색정보 받아올 수 있도록 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { IoIosSearch } from "react-icons/io";
+import { BsYoutube } from "react-icons/bs";
 
 export default function Header() {
+    const { text } = useParams();
     const navigate = useNavigate();
     const [search, setSearch] = useState('');
     const handleChange = (e) => setSearch(e.target.value);
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate(`/search/${search}`);
+        navigate(`/videos/${search}`);
     }
+
+    useEffect(() => {
+        setSearch(text || '');
+    },[text])
 
     return (
         <header className='flex justify-between p-5 mb-2'>
-            <Link to='/'>
-                <img
-                    className=''
-                    src='Youtubelogo.png'
-                    alt='유튜브로고'
-                    width='100px'
-                />
+            <Link to='/' className='flex justify-center items-center gap-2 font-bold text-3xl tracking-tighter uppercase'>
+                <BsYoutube className='text-red-600 text-4xl' />
+                <h1>Youtube</h1>
             </Link>
             <form
                 className='flex w-96'
